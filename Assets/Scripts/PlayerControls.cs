1ae8c096bcd23eacdfbc2044cc73015e87ed4dd1
DynamicDonut <bwilldonut@gmail.com>
2016-07-06 22:41:06 -0400

    Initial Commit

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	float hAxis, vAxis;
+	public float spd = 0.1f;
+	public string myName;
+	public bool isMoving = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+	void Move (){
+		if (Input.GetAxis ("Horizontal") != 0f || Input.GetAxis ("Vertical") != 0f) {
+			isMoving = true;
+		} else {
+			isMoving = false;
+		}
+
+		hAxis = Input.GetAxis ("Horizontal") * spd;
+		vAxis = Input.GetAxis ("Vertical") * spd;
+		transform.position = new Vector3 (transform.position.x + hAxis, transform.position.y + vAxis);
+	}
+}
