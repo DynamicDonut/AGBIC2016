fd8d9e47234e44d65e753e47091bd857c33c5a4e
DynamicDonut <bwilldonut@gmail.com>
2016-07-13 03:12:48 -0400

    Update #2
    Updated Spritesheet, thus animations as well
    
    Redid Speed and Movement, Speed builds up at 3 intervals and movement
    feels more binary.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,29 +6,28 @@ public class PlayerControls : MonoBehaviour {
 	public enum Direction { Up, UpLeft, Left, DownLeft, Down, DownRight, Right, UpRight }
 	//-----------------------0-----1-----2-------3--------4------5---------6-------7----
 	float sTime_Spd;
-	float spd_IncTime = 1.5f; // X seconds to increase speed while running
-	float spd_DecTime = 0.5f; // X seconds to decrease speed while running
+	//float spd_IncTime = 1.5f; // X seconds to increase speed while running
+	//float spd_DecTime = 0.5f; // X seconds to decrease speed while running
+
+	public float[] spd_IncTimeSet = new float[2]; //0 = Time to Fast2, 1 = Time to Fast3
+	public float[] spd_DecTimeSet = new float[2]; //0 = Time to Fast2, 1 = Time to Fast1
+	public float[] spdSet = new float[3]; //0 = Fast1, 1 = Fast2, 2 = Fast3
+	public int currSpd;
 
 	float hAxis, vAxis, defSpd, maxSpd, angle;
 	Animator anim;
-	public float spd = 0.1f;
-	public float spdInc = 0.045f;
-	public float spdDec = 0.045f;
 	public float stickDZone = 0.5f;
 	public string myName;
 	public bool isMoving = false;
 	Vector3 currPos, lastPos;
 	public Direction myDir = Direction.Up;
 
-	// Use this for initialization
 	void Start () {
-		defSpd = spd; // Base speed the player starts at.
-		maxSpd = 1.2f;
 		lastPos = transform.position;
+		currSpd = 0;
 		anim = GetComponent<Animator> ();
 	}
 	
-	// Update is called once per frame
 	void FixedUpdate () {
 		Move ();
 		SpeedManagement ();
@@ -45,10 +44,22 @@ public class PlayerControls : MonoBehaviour {
 		}
 		transform.rotation = Quaternion.Euler(Vector3.forward * angle);
 		*/
+
 		SpriteRotation();
 
 		//-------CHARACTER MOVEMENT-------//
 		currPos = transform.position;
+		if (currPos != lastPos) {
+			if (!isMoving) {
+				sTime_Spd = Time.time;
+			}
+			isMoving = true;
+		} else {
+			isMoving = false;
+			currSpd = 0;
+		}
+
+		/*
 		if (Mathf.Abs(Input.GetAxis ("Horizontal")) >= stickDZone|| Mathf.Abs(Input.GetAxis ("Vertical")) >= stickDZone) {
 			if (!isMoving) { sTime_Spd = Time.time; }
 			if (currPos != lastPos) {
@@ -57,11 +68,13 @@ public class PlayerControls : MonoBehaviour {
 		} else {
 			if (isMoving) { sTime_Spd = Time.time; }
 			isMoving = false;
+			currSpd = 0;
 		}
 
-		hAxis = Input.GetAxis ("Horizontal") * spd;
-		vAxis = Input.GetAxis ("Vertical") * spd;
-		transform.position = new Vector3 (transform.position.x + hAxis, transform.position.y + vAxis);
+		hAxis = Input.GetAxis ("Horizontal") * spdSet[currSpd];
+		vAxis = Input.GetAxis ("Vertical") * spdSet[currSpd];
+		*/
+		transform.position = new Vector3 (transform.position.x + hAxis * spdSet[currSpd], transform.position.y + vAxis * spdSet[currSpd]);
 	}
 
 	void SpriteRotation(){
@@ -91,9 +104,10 @@ public class PlayerControls : MonoBehaviour {
 			}				
 		}
 	}
-
+		
 	void SpeedManagement(){
 		if (isMoving) {
+			/*
 			if (Time.time > sTime_Spd + spd_IncTime) {
 				sTime_Spd += spd_IncTime;
 				if (spd < maxSpd) {
@@ -110,7 +124,49 @@ public class PlayerControls : MonoBehaviour {
 				} else {
 					spd = defSpd;
 				}
-			} 
+			}
+			*/
+
+			if (Time.time > sTime_Spd + spd_IncTimeSet[Mathf.Clamp(currSpd-1, 0, spdSet.Length)] && currSpd < 2){
+				sTime_Spd += spd_IncTimeSet[Mathf.Clamp(currSpd-1, 0, spdSet.Length)];
+				if (currSpd < 2) {
+					currSpd++;
+				} else {
+					currSpd = 2;
+				}
+			}
+		}
+
+		vAxis = 0f;
+		hAxis = 0f;
+
+		if (myDir == Direction.Up) {
+			vAxis = 1f;
+		} else if (myDir == Direction.Down) {
+			vAxis = -1f;
+		} 
+
+		if (myDir == Direction.Left) {
+			hAxis = -1f;
+		} else if (myDir == Direction.Right) {
+			hAxis = 1f;
+		} 
+
+		if (myDir == Direction.UpLeft) {
+			vAxis = 1f;
+			hAxis = -1f;
+		}
+		if (myDir == Direction.UpRight) {
+			vAxis = 1f;
+			hAxis = 1f;
+		}
+		if (myDir == Direction.DownLeft) {
+			vAxis = -1f;
+			hAxis = -1f;
+		}
+		if (myDir == Direction.DownRight) {
+			vAxis = -1f;
+			hAxis = 1f;
 		}
 	}
 
